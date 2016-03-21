c70f1d01107df3724c8d7a347757788fb195c6f0
Yunus Nedim Mehel <yunus.mehel@monitise.com>
2016-03-21 21:30:53 +0200

    Remove jsonAcc folder (Models are already in the project under /Networking)

--- jsonAcc/DataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by Yunus Nedim Mehel on 20/03/2016
-//  Copyright (c) 2016 __MyCompanyName__. All rights reserved.
-//
-
-#import "FTCPhoto.h"#import "FTCFTCBaseModel.h"#import "FTCPhotos.h"
